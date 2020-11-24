2e60fc1cea8c00dfbe9c1c3cd9b06596d51ae358
peppocola <giuseppecolavito999@gmail.com>
2020-11-24 18:01:16 +0100

    Update myzip.hs

--- myzip.hs
+++ myzip.hs
@@ -1,5 +1,5 @@
 module MyZip where
 
-myzip :: [x] -> [y] -> [(x,y)]
+myzip :: [x] -> [y] -> [(x,y)]              --This means that if I get two lists in input, one of type x and another of type y, in output I get a list of pairs where the first element is of type x and the second element is of type y
 myzip (x:xs) (y:ys) = (x,y) : myzip xs ys   --If this pattern matches, it means that both the lists have at least one element
 myzip _ _ = []                              --This pattern is mathched only if the first doesnt match, and this means that at least one of the two lists is empty. So we cut off other values of the non empty list.
